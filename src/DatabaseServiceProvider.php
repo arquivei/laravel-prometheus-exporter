86cf2f5e2f220bac3ba39db263b299d16c2fd49e
Mohamed Abdelmohsen <mohamed.abdelmohsen@careem.com>
2020-07-22 10:28:18 +0400

    clean the hard coded in() values

--- src/DatabaseServiceProvider.php
+++ src/DatabaseServiceProvider.php
@@ -69,6 +69,7 @@ class DatabaseServiceProvider extends ServiceProvider
     {
         $sql = preg_replace('/(VALUES\s*)(\([^\)]*+\)[,\s]*+)++/i', '$1()', $sql);
         $sql = preg_replace('/(\s*\?\s*,?\s*){2,}/i', '?', $sql);
+        $sql = preg_replace('/\(([0-9\s*,]+)\)/i', '(?)', $sql);
         $sql = str_replace('"', '', $sql);
 
         return empty($sql) ? '[error]' : $sql;

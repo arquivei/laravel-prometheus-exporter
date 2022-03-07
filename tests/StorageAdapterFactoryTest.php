aad709242aa3fe0320a7d57c7970ff9038e6f816
Rodrigo Roque <rodrigo.roque@arquivei.com.br>
2022-03-07 15:24:44 -0300

    Adds support for Laravel 9
    - Update dependencies
    - Update publish.yml
    - Removes support for versions older than PHP 8.0
    - Removes support for versions older than Laravel 9

--- tests/StorageAdapterFactoryTest.php
+++ tests/StorageAdapterFactoryTest.php
@@ -1,11 +1,12 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Arquivei\LaravelPrometheusExporter\Tests;
 
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use Prometheus\Exception\StorageException;
 use Prometheus\Storage\APC;
 use Prometheus\Storage\InMemory;
 use Prometheus\Storage\Redis;
@@ -21,31 +22,38 @@ class StorageAdapterFactoryTest extends TestCase
      */
     private $factory;
 
-    public function setUp() : void
+    public function setUp(): void
     {
         parent::setUp();
         $this->factory = new StorageAdapterFactory();
     }
 
-    public function testMakeMemoryAdapter() : void
+    public function testMakeMemoryAdapter(): void
     {
         $adapter = $this->factory->make('memory');
         $this->assertInstanceOf(InMemory::class, $adapter);
     }
 
-    public function testMakeApcAdapter() : void
+    public function testMakeApcAdapter(): void
     {
+        if (!extension_loaded('apcu')) {
+            $this->expectExceptionObject(new StorageException('APCu extension is not loaded'));
+        }
+        if (\function_exists('apcu_enabled') && !apcu_enabled()) {
+            $this->expectExceptionObject(new StorageException('APCu is not enabled'));
+        }
+
         $adapter = $this->factory->make('apc');
         $this->assertInstanceOf(APC::class, $adapter);
     }
 
-    public function testMakeRedisAdapter() : void
+    public function testMakeRedisAdapter(): void
     {
         $adapter = $this->factory->make('redis');
         $this->assertInstanceOf(Redis::class, $adapter);
     }
 
-    public function testMakeInvalidAdapter() : void
+    public function testMakeInvalidAdapter(): void
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('The driver [moo] is not supported.');
